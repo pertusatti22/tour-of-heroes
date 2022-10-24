53270535428abd3a39c9de22c0d0c0a438e95b7d
pertusatti22 <pertusatti22@gmail.com>
2022-10-24 14:30:43 -0300

    Add 2. Display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Strange' },
+  { id: 13, name: 'Captain America' },
+  { id: 14, name: 'Iron Man' },
+  { id: 15, name: 'Hulk' },
+  { id: 16, name: 'Spider-man' },
+  { id: 17, name: 'Thor' },
+  { id: 18, name: 'Black Panther' },
+  { id: 19, name: 'Captain Marvel' },
+  { id: 20, name: 'Star Lord' }
+];
